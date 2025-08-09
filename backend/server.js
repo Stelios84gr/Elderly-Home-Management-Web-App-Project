ab10eb681f9164ca2581c1a7abbbcfe3aa6b33e6
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-08-09 19:34:51 +0300

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const app = require('./app')
+const port = 3000;
+
+mongoose.connect(process.env.MONGODB_URI).then(
+    () => {
+        console.log("Successful connection to MongoDB!")
+    },
+    err => { console.log("Connection to MongoDB failed.", err); }
+)
+
+app.listen(port, () => {
+    console.log("Server is up")
+});
