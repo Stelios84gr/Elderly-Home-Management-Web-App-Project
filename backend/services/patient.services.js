c08a5d06870ee2b6eaad5e049f3049d03845bfb1
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-08-13 23:02:05 +0300

    feat: services added - findOne & findAll functions added

--- backend/services/patient.services.js
+++ backend/services/patient.services.js
@@ -3,4 +3,11 @@ const Patient = require('../models/patient.model')
 function findAll() {
     const result = Patient.find();
     return result;
+};
+
+function findOne(lastName) {
+    const result = UserActivation.findOne({lastName:lastName});
+    return result;
 }
+
+module.exports = { findAll, findOne }
