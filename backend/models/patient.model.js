3733f013936186b67b792296484eaf4de40bc665
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-08-15 00:13:40 +0300

    fix: removed uppercase option from firstName & lastName fields, removed unique option from visitor _id and added it to AMKA field

--- backend/models/patient.model.js
+++ backend/models/patient.model.js
@@ -13,11 +13,11 @@ let patientAilmentsSchema = new schema({
 }, {_id: false});
 
 let emergencyContactInfoSchema = new schema({
-    firstName: { type: String, required: [true, '"firstName" is a required field'], minLength: 2, maxLength: 20, trim: true, lowercase: true },
-    lastName: { type: String, required: [true, '"lastName" is a required field'], minLength: 2, maxLength: 20, trim: true, lowercase: true },
+    firstName: { type: String, required: [true, '"firstName" is a required field'], minLength: 2, maxLength: 20, trim: true },
+    lastName: { type: String, required: [true, '"lastName" is a required field'], minLength: 2, maxLength: 20, trim: true },
     phoneNumber: { type: Number, required: [true, '"phoneNumber" is a required field'], min: 1000000000, max: 9999999999 },
     address: {
-        road: { type: String, minLength: 2, maxLength: 20, trim: true, lowercase: true },
+        road: { type: String, minLength: 2, maxLength: 20, trim: true },
         number: { type: String, minLength: 1, maxLength: 5, trim: true, uppercase: true }    // in case of "1150A", for example
     },
     kinshipDegree: { type: String, required: [true, '"kinshipDegree" is a required field'], minLength: 4, maxLength: 15, trim: true, lowercase: true },
@@ -29,12 +29,12 @@ let visitorsSchema = new schema({   // both nested fields aren't required, as vi
 })
 
 let patientSchema = new schema({
-    _id: { type: String, required: [true, '"id" is a required field']},
+    _id: { type: String, required: [true, '"id" is a required field'], lowercase: true },
     firstName: { type: String, required: [true, '"firstName" is a required field'], minLength: 2, maxLength: 20, trim: true },
     lastName: { type: String, required: [true, '"lastName" is a required field'], minLength: 2, maxLength: 20, trim: true },
-    AMKA: { type: String, required: [true, '"AMKA" is a required field'], minLength: 11, maxLength: 11, trim: true },   // in case it begins with 0
+    AMKA: { type: String, required: [true, '"AMKA" is a required field'], minLength: 11, maxLength: 11, trim: true, unique: true },   // in case it begins with 0
     dateOfBirth: { type: Date, required: [true, '"dateOfBirth" is a required field'] },
-    phoneNumber: { type: Number, required: [true, '"phoneNumber" is a required field'], min: 1000000000, max: 9999999999},
+    phoneNumber: { type: Number, required: [true, '"phoneNumber" is a required field'], min: 1000000000, max: 9999999999, unique: true },
     authorizationToLeave: { type: Boolean, required: [true, '"authorizationToLeave" is a required field'] },
     roomData: roomDataSchema,
     patientAilments: [patientAilmentsSchema],    // array because there might be more than one
