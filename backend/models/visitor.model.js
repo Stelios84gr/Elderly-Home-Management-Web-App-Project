fd3e3bfb2424110e53c9555f0e638b8259c746a0
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-08-15 00:26:24 +0300

    fix: removed lowercase from address road, firstName and lastName fields, added _id field

--- backend/models/visitor.model.js
+++ backend/models/visitor.model.js
@@ -3,13 +3,14 @@ const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 
 let addressSchema = new schema({
-    road: { type: String, minLength: 2, maxLength: 20, trim: true, lowercase: true },
+    road: { type: String, minLength: 2, maxLength: 20, trim: true },
     number: { type: String, minLength: 1, maxLength: 5, trim: true, uppercase: true }    // in case of "1150A", for example
 })
 
 let visitorSchema = new schema({
-    firstName: { type: String, required: [true, '"firstName" is a required field'], minLength: 2, maxLength: 20, trim: true, lowercase: true },
-    lastName: { type: String, required: [true, '"lastName" is a required field'], minLength: 2, maxLength: 20, trim: true, lowercase: true },
+    _id: { type: String, required: [true, '"id" is a required field'] },
+    firstName: { type: String, required: [true, '"firstName" is a required field'], minLength: 2, maxLength: 20, trim: true },
+    lastName: { type: String, required: [true, '"lastName" is a required field'], minLength: 2, maxLength: 20, trim: true },
     phoneNumber: { type: Number, required: [true, '"phoneNumber" is a required field'], min: 1000000000, max: 9999999999 },
     address: addressSchema,
     relationship: { type: String, required: [true, "relationship is a required field"], minLength: 4, maxLength: 15, trim: true, lowercase: true },
