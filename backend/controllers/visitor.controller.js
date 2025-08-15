6578aaafe215df262829ee81a2696ab8501fd6d7
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-08-15 20:40:01 +0300

    feat: visitor controller added

---
+++ backend/controllers/visitor.controller.js
@@ -0,0 +1,102 @@
+const Visitor = require('../models/visitor.model');
+const visitorService = require('../services/visitor.services');
+
+exports.findAll = async(req, res) => {
+    console.log("Retrieve all visitors from 'visitors' collection.");
+
+    try {
+        // const result = await Visitor.find(); => delegated to visitor.service
+
+        const result = await visitorService.findAll()
+        res.status(200).json({status: true, data: result})
+    } catch (err) {
+        console.log("Error reading 'visitors' collection.", err)
+        res.status(400).json({ status: false, data: err})
+    };
+};
+
+exports.findOne = async(req, res) => {
+    console.log('Find a specific visitor.');
+    const id = req.params.id;    // find by id
+
+    try {
+        const result = await visitorService.findById(id);    // cleaner than findOne({_id:id})
+        // const result = await Visitor.findById({id}); => delegated to visitor.services
+        if (result) {   // not finding the visitor does not raise an error and go to catch
+            res.status(200).json({ status: true, data: result});
+        }   else {
+            res.status(404).json({ status: false, data: "Visitor not found."})
+        }
+    } catch (err) {
+        console.log('Error finding visitor.', err);
+        res.status(400).json({ status: false, data: err });
+    }
+}
+
+exports.create = async(req, res) => {
+    console.log('Create visitor.');
+
+    const data = req.body;
+
+    const newVisitor = new Visitor({
+        firstName: data.firstName,
+        lastName: data.lastName,
+        phoneNumber: data.phoneNumber,
+        address: {
+            road: data.address?.road,
+            number: data.address?.number
+        },
+        relationship: data.relationship,
+        isFamily: data.isFamily
+    });
+
+    try {
+        const result = await newVisitor.save();
+        res.status(200).json({ status: true, data: result});
+    } catch (err) {
+        console.log('Error creating visitor.', err);
+        res.status(400).json({ status: false, data: err});
+    }
+};
+
+exports.update = async(req, res) => {
+    const id = req.params.id;    // id will be retrieved from URL (path params)
+
+    console.log("Update viistor data by id: ", id, ".");
+
+    try {
+        const result = await Visitor.findByIdAndUpdate(
+            id,
+            { $set: req.body},    // only update the fields sent (PATCH) - ignore fields not included in schemas
+            {new: true, runValidators: true},    // runValidators applies validation checks also when updating
+        );
+        if (!result) {
+            return res.status(404).json({ status: false, data: "Visitor not found." })
+        }
+        res.status(200).json({ status: true, data: result });
+    }   catch (err) {
+        console.log("Error updating visitor:", err);
+        res.status(400).json({ status: false, data: err });
+    }
+};
+
+exports.deleteById = async (req, res) => {
+    const id = req.params.id;
+    console.log("Delete visitor with id: ", id, ".");
+
+    try {
+        const result = await Visitor.findByIdAndDelete(id);
+
+        // avoid returning status 200 - null if no visitor is found
+        if (!result) {
+            return res.status(404).json({
+                status: false, data: "Visitor not found."
+            });
+        }
+
+        res.status(200).json({ status: true, data: result })
+    } catch (err) {
+        console.log("Error deleting visitor.", err);
+        res.status(400).json({ status: false, data: err});
+    }
+};
