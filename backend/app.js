a2534791cd056206764f86538853cd7c4a93f703
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-08-16 21:50:53 +0300

    feat: auth middleware added & removed innactive ones

--- backend/app.js
+++ backend/app.js
@@ -6,10 +6,12 @@ app.use(express.json());    // express parses JSON files in HTTP requests
 const patient = require('./routes/patient.routes');
 const visitor = require( './routes/visitor.routes');
 const staff = require('./routes/staff.routes');
+const auth = require('./routes/auth.routes');
 
+app.use('/api/auth', auth)
 app.use('/api/patients', patient);
-app.use('/api/staff', staff);
-app.use('/api/visitors', visitor);
+// app.use('/api/staff', staff);
+// app.use('/api/visitors', visitor);
 
 
 module.exports = app
