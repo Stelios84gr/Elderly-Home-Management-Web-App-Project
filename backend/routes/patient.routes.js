05b4e8aec6c396a25dc9f36870dcdefbcc5eed41
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-08-18 22:32:20 +0300

    feat: middlewares added

--- backend/routes/patient.routes.js
+++ backend/routes/patient.routes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const patientController = require('../controllers/patient.controller');    // assigns all methods from patients.controller.js
+const patientController = require('../controllers/patient.controller');
+const verifyToken = require('../middlewares/auth.middleware').verifyToken;
+const verifyRoles = require('../middlewares/auth.middleware').verifyRoles;
 
-router.get('/', patientController.findAll);
-router.get('/:id', patientController.findOne);
-router.post('/', patientController.create);
-router.patch('/:id', patientController.update);
-router.delete('/:id', patientController.deleteById)
+router.get('/', verifyToken, patientController.findAll);
+router.get('/:id', verifyToken, verifyRoles(["EDITOR", "ADMIN"]), patientController.findOne);
+router.post('/', verifyToken, verifyRoles("ADMIN"), patientController.create);
+router.patch('/:id', verifyToken, verifyRoles("ADMIN"),  patientController.update);
+router.delete('/:id', verifyToken, verifyRoles("ADMIN"), patientController.deleteById);
 
 module.exports = router;
--- backend/services/auth.service.js
+++ backend/services/auth.service.js
@@ -1,7 +1,8 @@
 const jwt = require('jsonwebtoken');
 
 function generateAccessToken(staff){
-    console.log('Auth Service: ', staff)
+    console.log('Auth Service: ', staff);
+
     const payload = {
         username: staff.username,
         roles: staff.roles
@@ -13,4 +14,15 @@ function generateAccessToken(staff){
     return jwt.sign(payload, secret, options);
 }
 
-module.exports = { generateAccessToken };
+function verifyAccessToken(token){
+    const secret = process.env.TOKEN_SECRET;
+    try {
+        const payload = jwt.verify(token, secret);
+        console.log("Token verified: ", payload);
+        return { verified: true, data: payload }
+    } catch (err) {
+        return { verified: false, data: err}
+    }
+}
+
+module.exports = { generateAccessToken, verifyAccessToken };
