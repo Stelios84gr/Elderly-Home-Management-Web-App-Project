e5c57f49351ad697eee34b554a29027ebbb1b20e
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-08-18 18:00:00 +0300

    feat: jwt authorization controller added

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,30 @@
+const bcrypt = require('bcrypt');
+const Staff = require('../models/staff.model');
+const authService = require('../services/auth.service');
+
+exports.login = async(req, res) => {
+    console.log("Login user", req.body);
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    try {
+        const result = await Staff.findOne({ username: username });
+
+        if (!result) {
+            return res.status(404).json({ status: false, data: "User not found." });
+        }
+    
+    const isMatch = await bcrypt.compare(password, result.password);
+         
+        if (isMatch) {
+            const token = authService.generateAccessToken(result);
+            return res.status(200).json({ status: true, data: token}); 
+        } else {
+            return res.status(401).json({ status: false, data: "Invalid credentials."});
+        }
+    } catch (err) {
+        console.log("Problem logging in.", err);
+        return res.status(400).json({ status: false, data: err });
+    }
+}
