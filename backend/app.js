779e7fb4ac226502c3c1216a8b80fd817f9bb323
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-08-19 20:58:42 +0300

    feat: Swagger route mounting statement added

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,9 @@ const app = express();
 
 app.use(express.json());
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger');
+
 const patient = require('./routes/patient.routes');
 const visitor = require( './routes/visitor.routes');
 const staff = require('./routes/staff.routes');
@@ -13,5 +16,10 @@ app.use('/api/patients', patient);
 app.use('/api/staff', staff);
 app.use('/api/visitors', visitor);
 
+app.use(
+    '/api-docs/',
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument.options)
+);
 
 module.exports = app
