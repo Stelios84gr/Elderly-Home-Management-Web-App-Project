caf0a88417f1c367443e0f7684f8f0ad89a21120
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-08-20 01:38:20 +0300

    fix: all role values inserted correctly in arrays

--- backend/routes/patient.routes.js
+++ backend/routes/patient.routes.js
@@ -7,8 +7,8 @@ const verifyRoles = require('../middlewares/auth.middleware').verifyRoles;
 
 router.get('/', verifyToken, patientController.findAll);
 router.get('/:username', verifyToken, verifyRoles(["EDITOR", "ADMIN"]), patientController.findOne);
-router.post('/', verifyToken, verifyRoles("ADMIN"), patientController.create);
-router.patch('/:username', verifyToken, verifyRoles("ADMIN"),  patientController.update);
-router.delete('/:username', verifyToken, verifyRoles("ADMIN"), patientController.delete);
+router.post('/', verifyToken, verifyRoles(["ADMIN"]), patientController.create);
+router.patch('/:username', verifyToken, verifyRoles(["ADMIN"]),  patientController.update);
+router.delete('/:username', verifyToken, verifyRoles(["ADMIN"]), patientController.delete);
 
 module.exports = router;
