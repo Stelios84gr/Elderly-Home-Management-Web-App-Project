2af0a1f402a15f935fe721d3c519e41d9ed71b72
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-08-19 18:05:18 +0300

    feat: more endpoints added

--- backend/routes/staff.routes.js
+++ backend/routes/staff.routes.js
@@ -1,4 +1,14 @@
 const express = require('express')
 const router = express.Router();
 
-// const staffController = 
+const staffController = require('../controllers/staff.controller');
+const verifyToken = require('../middlewares/auth.middleware').verifyToken;
+const verifyRoles = require('../middlewares/auth.middleware').verifyRoles;
+
+router.get('/', verifyToken, staffController.findAll);
+router.get('/:id', verifyToken, verifyRoles(["EDITOR", "ADMIN"]), staffController.findOne);
+router.post('/', verifyToken, verifyRoles("ADMIN"), staffController.create);
+router.patch('/:id', verifyToken, verifyRoles("ADMIN"),  staffController.update);
+router.delete('/:id', verifyToken, verifyRoles("ADMIN"), staffController.deleteById);
+
+module.exports = router;
--- backend/routes/visitor.routes.js
+++ backend/routes/visitor.routes.js
@@ -1,4 +1,14 @@
 const express = require('express')
 const router = express.Router();
 
-// const visitorController = 
+const visitorController = require('../controllers/visitor.controller');
+const verifyToken = require('../middlewares/auth.middleware').verifyToken;
+const verifyRoles = require('../middlewares/auth.middleware').verifyRoles;
+
+router.get('/', verifyToken, visitorController.findAll);
+router.get('/:id', verifyToken, verifyRoles(["EDITOR", "ADMIN"]), visitorController.findOne);
+router.post('/', verifyToken, verifyRoles("ADMIN"), visitorController.create);
+router.patch('/:id', verifyToken, verifyRoles("ADMIN"),  visitorController.update);
+router.delete('/:id', verifyToken, verifyRoles("ADMIN"), visitorController.deleteById);
+
+module.exports = router;
