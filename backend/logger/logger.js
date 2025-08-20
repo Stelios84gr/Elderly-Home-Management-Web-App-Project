8d15a1aade4d967a2654d2e4973a224d11f792f9
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-08-20 18:53:58 +0300

    feat: logger created

--- backend/logger/logger.js
+++ backend/logger/logger.js
@@ -1,12 +1,33 @@
-const winston = require('winston');
-const logger = winston.createLogger(
-    {
-        format: winston.format.json(),
-        transports: [
-            new winston.transports.Console()
-        ]
+require('winston-daily-rotate-file');
+require('winston-mongodb');
+const { format, createLogger, transports } = require('winston');
+const { combine, timestamp, label, printf, prettyPrint } = format;
 
-    }
-)
+const logger = createLogger({
+    format: combine(
+        label({ label: "HYACINTH ELDERLY HOME PATIENTS LOGS" }),
+        timestamp({ format: "DD-MM-YYYY HH:mm:ss" }),
+        format.json(),
+        prettyPrint()
+    ),
+    transports: [
+        new transports.Console(),
+        new transports.DailyRotateFile({
+    filename: "./logs/rotate-%DATE%.log",
+    datePattern: "DD-MM-YYYY",
+    maxFiles: "7d",
+    level: "error"
+    }),
+    new transports.MongoDB({
+        level: "error",
+        db: process.env.MONGODB_URI,
+        collection: "error_logs",
+        format: format.combine(
+            format.timestamp(),
+            format.json()
+        )
+    })
+    ]
+});
 
 module.exports = logger;
