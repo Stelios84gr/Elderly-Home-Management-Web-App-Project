afb359dfbb4841a0d7864cf0bef189e9c8a2ad5a
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-08-20 19:54:20 +0300

    fix: custom format and transport properly refactored

--- backend/logger/logger.js
+++ backend/logger/logger.js
@@ -1,14 +1,22 @@
 require('winston-daily-rotate-file');
 require('winston-mongodb');
 const { format, createLogger, transports } = require('winston');
-const { combine, timestamp, label, printf, prettyPrint } = format;
+const { combine, timestamp, label, printf } = format;
 
 const logger = createLogger({
     format: combine(
         label({ label: "HYACINTH ELDERLY HOME PATIENTS LOGS" }),
         timestamp({ format: "DD-MM-YYYY HH:mm:ss" }),
-        format.json(),
-        prettyPrint()
+        // printf: use custom order of combine options
+        // "fake" prettyPrint by using template literals
+        printf(({ level, message, label, timestamp}) => {
+            return `{
+    label: '${label}',
+    message: '${message}'.
+    level: '${level}',
+    timestamp: '${timestamp}'
+}`;
+        })
     ),
     transports: [
         new transports.Console(),
@@ -18,6 +26,7 @@ const logger = createLogger({
     maxFiles: "7d",
     level: "error"
     }),
+    // stores correct timestamp in metadata (default MongoDB Compass time zone: UTC)
     new transports.MongoDB({
         level: "error",
         db: process.env.MONGODB_URI,
