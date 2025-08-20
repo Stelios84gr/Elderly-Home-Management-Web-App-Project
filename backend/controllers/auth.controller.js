46fb3f82ba54b52894767e69fb5352f4fd18fa40
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-08-20 19:53:51 +0300

    feat: loggers added

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt');
 const Staff = require('../models/staff.model');
 const authService = require('../services/auth.service');
+const logger = require('../logger/logger');
 
 exports.login = async(req, res) => {
     console.log("Login user", req.body);
@@ -12,6 +13,7 @@ exports.login = async(req, res) => {
         const result = await Staff.findOne({ username: username }, {username: 1, password: 1, roles: 1});
 
         if (!result) {
+            logger.error("Error finding user.");
             return res.status(404).json({ status: false, data: "User not found." });
         }
     
@@ -21,10 +23,12 @@ exports.login = async(req, res) => {
             const token = authService.generateAccessToken(result);
             return res.status(200).json({ status: true, data: token}); 
         } else {
+            logger.error("Error: Invalid credentials.");
             return res.status(401).json({ status: false, data: "Invalid credentials."});
         }
     } catch (err) {
         console.log("Problem logging in.", err);
+        logger.error("Error logging in: ", err);
         return res.status(400).json({ status: false, data: err });
     }
 }
