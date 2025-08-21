43330182efe607479b3e1775f3c51ee386b4ffe1
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-08-21 21:11:19 +0300

    chore: google sign-in url code comments added

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -6,3 +6,15 @@ router.post('/login', authController.login);
 router.get('/google/callback', authController.googleLogin);
 
 module.exports = router;
+
+// Google Sign-In URL:
+// https://accounts.google.com/o/oauth2/auth + path parameters
+
+// path parameters:
+// client_id = ...
+// redirect_uri = ...
+// response_type = code
+// scope = email%20profile
+// access_type = offline
+
+// https://accounts.google.com/o/oauth2/auth?client_id=[...]]&redirect_uri=[...]&response_type=code&scope=email%20profile&access_type=offline
