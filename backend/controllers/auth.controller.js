8e721e15d77a83d9c044f8a528f5eb52fb65705b
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-08-21 19:44:14 +0300

    feat: Google Sign-In method added

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -31,4 +31,18 @@ exports.login = async(req, res) => {
         logger.error("Error logging in: ", err);
         return res.status(400).json({ status: false, data: err });
     }
-}
+};
+
+exports.googleLogin = async(req, res) => {
+    const code = req.query.code;
+    if (!code) {
+        res.status(400).json({ status: false, data: "Authorization code missing." });
+    } else {
+        let user = await authService.googleAuth(code);
+        if (user) {
+            res.status(200).json({ status: true, data: user });
+        } else {
+            res.status(400).json({ status: false, data: "Problem logging-in with user." });
+        };
+    };
+};
