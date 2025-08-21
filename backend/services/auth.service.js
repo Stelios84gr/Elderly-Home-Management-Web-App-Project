1ea66892af83470da3d6515c5eae4d00c66c4b28
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-08-21 19:44:44 +0300

    feat: googleAuth method added

--- backend/services/auth.service.js
+++ backend/services/auth.service.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const { OAuth2Client } = require('google-auth-library');
 
 function generateAccessToken(staff){
     console.log('Auth Service: ', staff);
@@ -6,13 +7,13 @@ function generateAccessToken(staff){
     const payload = {
         username: staff.username,
         roles: staff.roles
-    }
+    };
 
     const secret = process.env.TOKEN_SECRET;
     const options = { expiresIn: '1h' };
 
     return jwt.sign(payload, secret, options);
-}
+};
 
 function verifyAccessToken(token){
     const secret = process.env.TOKEN_SECRET;
@@ -22,7 +23,31 @@ function verifyAccessToken(token){
         return { verified: true, data: payload }
     } catch (err) {
         return { verified: false, data: err}
-    }
-}
+    };
+};
 
-module.exports = { generateAccessToken, verifyAccessToken };
+async function googleAuth(code) {
+    console.log("Google Sign-In");
+    const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+    const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+    const REDIRECT_URI = process.env.REDIRECT_URI;
+    const oAuth2Client = new OAuth2Client(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, REDIRECT_URI);
+
+    try {
+        const { tokens } = await oAuth2Client.getToken(code);
+        oAuth2Client.setCredentials(tokens);
+
+        const ticket = await oAuth2Client.verifyIdToken({
+            idToken: tokens.id_token,
+            audience: GOOGLE_CLIENT_ID
+        });
+
+        const userInfo = await ticket.getPayload();
+        return { user: userInfo, tokens };
+    } catch (err) {
+        console.error("Error during Google authentication:", err);
+        return { error: "Google authentication failed." };
+    };
+};
+
+module.exports = { generateAccessToken, verifyAccessToken, googleAuth };
