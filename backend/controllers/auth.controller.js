196bf52282a9ba5ab2910dede09745c1a7b6ca44
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-08-21 20:07:49 +0300

    minor: semicolons added & some response messages changed

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -15,7 +15,7 @@ exports.login = async(req, res) => {
         if (!result) {
             logger.error("Error finding user.");
             return res.status(404).json({ status: false, data: "User not found." });
-        }
+        };
     
     const isMatch = await bcrypt.compare(password, result.password);
          
@@ -25,24 +25,24 @@ exports.login = async(req, res) => {
         } else {
             logger.error("Error: Invalid credentials.");
             return res.status(401).json({ status: false, data: "Invalid credentials."});
-        }
+        };
     } catch (err) {
         console.log("Problem logging in.", err);
         logger.error("Error logging in: ", err);
         return res.status(400).json({ status: false, data: err });
-    }
+    };
 };
 
 exports.googleLogin = async(req, res) => {
     const code = req.query.code;
     if (!code) {
-        res.status(400).json({ status: false, data: "Authorization code missing." });
+        res.status(400).json({ status: false, data: "Authorization code not returned." });
     } else {
         let user = await authService.googleAuth(code);
         if (user) {
             res.status(200).json({ status: true, data: user });
         } else {
-            res.status(400).json({ status: false, data: "Problem logging-in with user." });
+            res.status(400).json({ status: false, data: "Problem logging in with user." });
         };
     };
 };
