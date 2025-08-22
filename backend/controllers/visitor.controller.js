026c037d88b2c56e7104705f0ee157183fdba5c7
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-08-22 17:53:48 +0300

    fix: model import removed, logger import added, visitors now correctly referenced, id references changed to username, create method delegated to services

--- backend/controllers/visitor.controller.js
+++ backend/controllers/visitor.controller.js
@@ -1,5 +1,5 @@
-const Visitor = require('../models/visitor.model');
 const visitorService = require('../services/visitor.service');
+const logger = require('../logger/logger');
 
 exports.findAll = async(req, res) => {
     console.log("Retrieve all visitors from 'visitors' collection.");
@@ -12,19 +12,19 @@ exports.findAll = async(req, res) => {
     } catch (err) {
         console.log("Error reading 'visitors' collection.", err);
         res.status(400).json({ status: false, data: err});
-        logger.error("Error reading all patients.", err);
+        logger.error("Error reading all visitors.", err);
     };
 };
 
 exports.findOne = async(req, res) => {
     console.log('Find a specific visitor.');
-    const id = req.params.id;    // find by id
+    const username = req.params.username;    // find by username
 
     try {
-        const result = await visitorService.findById(id);    // cleaner than findOne({_id:id})
-        // const result = await Visitor.findById({id}); => delegated to visitor.service
+        const result = await visitorService.findOne(username);
+        // const result = await Visitor.findOne(username); => delegated to visitor.service
         if (result) {   // not finding the visitor does not raise an error and go to catch
-            res.status(200).json({ status: true, data: result});
+            res.status(201).json({ status: true, data: result});
         }   else {
             res.status(404).json({ status: false, data: "Visitor not found."});
             logger.error("Error finding visitor.");
@@ -41,20 +41,8 @@ exports.create = async(req, res) => {
 
     const data = req.body;
 
-    const newVisitor = new Visitor({
-        firstName: data.firstName,
-        lastName: data.lastName,
-        phoneNumber: data.phoneNumber,
-        address: {
-            road: data.address?.road,
-            number: data.address?.number
-        },
-        relationship: data.relationship,
-        isFamily: data.isFamily
-    });
-
     try {
-        const result = await newVisitor.save();
+        const result = await visitorService.create(data);
         res.status(200).json({ status: true, data: result});
     } catch (err) {
         console.log('Error creating visitor.', err);
@@ -64,16 +52,14 @@ exports.create = async(req, res) => {
 };
 
 exports.update = async(req, res) => {
-    const id = req.params.id;    // id will be retrieved from URL (path params)
+    const username = req.params.username;    // username will be retrieved from URL (path params)
 
-    console.log("Update viistor data by id: ", id, ".");
+    console.log("Update viistor data by username: ", username, ".");
+
+    const data = req.body;
 
     try {
-        const result = await Visitor.findByIdAndUpdate(
-            id,
-            { $set: req.body},    // only update the fields sent (PATCH) - ignore fields not included in schemas
-            {new: true, runValidators: true},    // runValidators applies validation checks also when updating
-        );
+        const result = await visitorService.update(username, data);
         if (!result) {
             logger.error("Error finding visitor.");
             return res.status(404).json({ status: false, data: "Visitor not found." })
@@ -82,29 +68,29 @@ exports.update = async(req, res) => {
     }   catch (err) {
         console.log("Error updating visitor.", err);
         res.status(400).json({ status: false, data: err });
-         logger.error("Error updating patient.", err);
+        logger.error("Error updating visitor.", err);
     }
 };
 
-exports.deleteById = async (req, res) => {
-    const id = req.params.id;
-    console.log("Delete visitor with id: ", id, ".");
+exports.deleteByUsername = async (req, res) => {
+    const username = req.params.username;
+    console.log("Delete visitor with username: ", username, ".");
 
     try {
-        const result = await Visitor.findByIdAndDelete(id);
+        const result = await visitorService.deleteByUsername(username);
 
-        // avoid returning status 200 - null if no visitor is found
+        // avoid returning status 200 & null if no visitor is found
         if (!result) {
             logger.error("Visitor not found");
             return res.status(404).json({
-                status: false, data: "Visitor not found."
+            status: false, data: "Visitor not found."
             });
-        }
+        };
 
         res.status(200).json({ status: true, data: result })
     } catch (err) {
         console.log("Error deleting visitor.", err);
         res.status(400).json({ status: false, data: err});
-        logger.error("Error deleting patient.", err);
-    }
+        logger.error("Error deleting visitor.", err);
+    };
 };
