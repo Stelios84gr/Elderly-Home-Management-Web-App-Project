363b6fccb3c8654eae660d43c12cfdd329e86b7f
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-08-23 18:25:08 +0300

    fix: roles in verifyRoles now properly enclosed in array, deletebyId controller reference changed to deletebyUsername

--- backend/routes/visitor.routes.js
+++ backend/routes/visitor.routes.js
@@ -7,8 +7,8 @@ const verifyRoles = require('../middlewares/auth.middleware').verifyRoles;
 
 router.get('/', verifyToken, visitorController.findAll);
 router.get('/:username', verifyToken, verifyRoles(["EDITOR", "ADMIN"]), visitorController.findOne);
-router.post('/', verifyToken, verifyRoles("ADMIN"), visitorController.create);
-router.patch('/:username', verifyToken, verifyRoles("ADMIN"),  visitorController.update);
-router.delete('/:username', verifyToken, verifyRoles("ADMIN"), visitorController.deleteById);
+router.post('/', verifyToken, verifyRoles(["ADMIN"]), visitorController.create);
+router.patch('/:username', verifyToken, verifyRoles(["ADMIN"]),  visitorController.update);
+router.delete('/:username', verifyToken, verifyRoles(["ADMIN"]), visitorController.deleteByUsername);
 
 module.exports = router;
