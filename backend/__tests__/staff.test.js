7267cc0458a2109df8867f02906f550ed014a452
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-08-23 17:20:51 +0300

    feat: mock visitor before tests, more clean-ups after tests and timeouts added

--- backend/__tests__/staff.test.js
+++ backend/__tests__/staff.test.js
@@ -27,10 +27,36 @@ afterAll(async () => {
     await mongoose.connection.close();
 });
 
+beforeEach(async () => {
+    // check that the staff member doesn't already exist
+    const existingStaffMember = await staffService.findOne("test0");
+
+    if (!existingStaffMember) {
+        await staffService.create({
+            username: "test0",
+            password: "p4Sw0rd!0",
+            firstName: "testFN0",
+            lastName: "testLN0",
+            AMKA: "00112233456",
+            phoneNumber: 6987654321,
+            address: {
+            road: "testRoad0",
+            number: 50
+            },
+            occupation: "Nurse",
+            roles: ["READER"],
+            startDate: "2018-09-10 08:00:00",
+            monthlySalary: 1020
+        });
+    };
+});
+
 // clean up mock POST documents
 afterEach(async () => {
     await staffService.deleteByUsername("test0");
     await staffService.deleteByUsername("test2");
+    await staffService.deleteByUsername("test3");
+    await staffService.deleteByUsername("deleteMe");  // in case DELETE test fails
 });
 
 describe("Requests for /api/staff", () => {
@@ -45,23 +71,23 @@ describe("Requests for /api/staff", () => {
     expect(res.body.data.length).toBeGreaterThan(0);
     }, 5000);    
 
-    test("POST - Creates staff member", async () => {
+    test("POST - Creates a staff member", async () => {
     const res = await request(app)
         .post("/api/staff")
         .set("Authorization", `Bearer ${token}`)
         .send({
-        "username": "staffTest1",
-        "password": "Password123",
-        "firstName": "sFN",
-        "lastName": "sLN",
-        "TIN": "123456789",
-        "phoneNumber": "6912345678",
-        "occupation": "Nurse",
-        "monthlySalary": 1200,
-        "address": {
-            "road": "Main",
-            "number": 10
-        }
+            "username": "test3",
+            "password": "p4Sw0rd!3",
+            "firstName": "sFN",
+            "lastName": "sLN",
+            "TIN": "123456789",
+            "phoneNumber": 6912345678,
+            "occupation": "Nurse",
+            "monthlySalary": 1200,
+            "address": {
+                "road": "Main",
+                "number": 10
+            }
         });
 
     expect(res.statusCode).toBe(201);
@@ -73,52 +99,54 @@ describe("Requests for /api/staff", () => {
         .post("/api/staff")
         .set("Authorization", `Bearer ${token}`)
         .send({
-            "username": "staffTest1",
-            "password": "Password124",
+            "username": "test0",
+            "password": "p4Sw0rd!1",
             "firstName": "rFN",
             "lastName": "rLN",
             "TIN": "123456782",
-            "phoneNumber": "6912345677",
+            "phoneNumber": 6912345677,
             "occupation": "Nurse",
             "monthlySalary": 1100,
             "address": {
                 "road": "Orchard",
                 "number": 8
-            }
+            },
+            "roles": ["READER"],
+            "startDate": "2019-05-20 08:00:00",
         });
 
         expect(res.statusCode).toBe(400);
         expect(res.body.status).not.toBeTruthy();
-    });
+    }, 5000);
 
     test("POST - Create staff with empty firstName, TIN and occupation", async () => {
     const res = await request(app)
         .post("/api/staff")
         .set("Authorization", `Bearer ${token}`)
         .send({
-        "username": "staffTest2",
-        "password": "Password123",
+        "username": "test2",
+        "password": "p4Sw0rd!2",
         "firstName": "",
         "lastName": "sLN2",
         "TIN": "",
-        "phoneNumber": "6923456789",
+        "phoneNumber": 6923456789,
         "occupation": "",
         "monthlySalary": 1300,
         "address": {
             "road": "Second",
             "number": 22
-        }
+        },
         });
 
         expect(res.statusCode).toBe(400);
         expect(res.body.status).not.toBeTruthy();
-    });
+    }, 5000);
 });
 
 describe("/api/staff/:username", () => {
 
     test("GET - Returns staff member by username", async () => {
-        const result = await staffService.findOne();
+        const result = await staffService.findOne("test0");
 
         const res = await request(app)
             .get("/api/staff/" + result.username)
@@ -127,10 +155,10 @@ describe("/api/staff/:username", () => {
         expect(res.statusCode).toBe(200);
         expect(res.body.status).toBeTruthy();
         expect(res.body.data.username).toBe(result.username);
-    });
+    }, 5000);
 
     test("PATCH - Updates staff member data", async () => {
-        const result = await staffService.findOne();
+        const result = await staffService.findOne("test0");
 
         // original values backup before updating
         const originalData = {
@@ -157,29 +185,29 @@ describe("/api/staff/:username", () => {
             .set('Authorization', `Bearer ${token}`)
             .send(originalData);
         };
-    });
+    }, 5000);
 
     test("PATCH - Updates staff member with empty fields", async () => {
-    const result = await staffService.findOne();
+        const result = await staffService.findOne("test0");
 
-    const res = await request(app)
-    .patch("/api/staff/" + result.username)
-    .set("Authorization", `Bearer ${token}`)
-    .send({
-    "username": result.username,
-    "TIN": "",
-    "phoneNumber": ""
-    });
+        const res = await request(app)
+        .patch("/api/staff/" + result.username)
+        .set("Authorization", `Bearer ${token}`)
+        .send({
+        "username": result.username,
+        "TIN": "",
+        "phoneNumber": ""
+        });
 
-    expect(res.statusCode).toBe(400);
-    expect(res.body.status).not.toBeTruthy();
-    });
+        expect(res.statusCode).toBe(400);
+        expect(res.body.status).not.toBeTruthy();
+    }, 5000);
 
     test("DELETE - Deletes staff member by username", async () => {
 
     // create test staff document to be deleted
         const testStaffMember = {
-            username: "delete_me",
+            username: "deleteMe",
             password: "S0m3th1ng!",
             firstName: "Del",
             lastName: "Ete",
@@ -203,7 +231,7 @@ describe("/api/staff/:username", () => {
 
         expect(res.statusCode).toBe(200);
         expect(res.body.status).toBeTruthy();
-    });
+    }, 5000);
 
     test("DELETE - Try to delete non-existent staff member", async () => {
 
@@ -214,4 +242,4 @@ describe("/api/staff/:username", () => {
     expect(res.statusCode).toBe(404);
     expect(res.body.status).not.toBeTruthy();
     });
-});
+}, 5000);
