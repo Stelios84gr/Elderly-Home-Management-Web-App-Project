abea603e11c143c92a93b6dc2edcc29e3323560d
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-08-29 23:52:39 +0300

    fix: changed routes order to correct "route shadowing"

--- backend/routes/staff.routes.js
+++ backend/routes/staff.routes.js
@@ -5,10 +5,9 @@ const staffController = require('../controllers/staff.controller');
 const verifyToken = require('../middlewares/auth.middleware').verifyToken;
 const verifyRoles = require('../middlewares/auth.middleware').verifyRoles;
 
-router.get('/', verifyToken, staffController.findAll);
+router.get('/', verifyToken, staffController.findAll);router.get('/checkDuplicateUsername/:username', staffController.checkDuplicateUsername);
+router.get('/checkDuplicateEmail/:email', staffController.checkDuplicateEmail);
 router.get('/:username', verifyToken, verifyRoles(["EDITOR", "ADMIN"]), staffController.findOne);
-router.get('/checkDuplicateUsername/:username', verifyToken, staffController.checkDuplicateUsername);
-router.get('/checkDuplicateEmail/:email', verifyToken, staffController.checkDuplicateEmail);
 router.post('/', verifyToken, verifyRoles("ADMIN"), staffController.create);
 router.patch('/:username', verifyToken, verifyRoles("ADMIN"),  staffController.update);
 router.delete('/:username', verifyToken, verifyRoles("ADMIN"), staffController.deleteByUsername);
