bf306b0d5b5a612ac1564d0b7cb30dae30606d7b
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-08-29 23:54:36 +0300

    fix: changed routes order to correct "route shadowing"

--- backend/routes/patient.routes.js
+++ backend/routes/patient.routes.js
@@ -6,8 +6,8 @@ const verifyToken = require('../middlewares/auth.middleware').verifyToken;
 const verifyRoles = require('../middlewares/auth.middleware').verifyRoles;
 
 router.get('/', verifyToken, patientController.findAll);
-router.get('/:username', verifyToken, verifyRoles(["EDITOR", "ADMIN"]), patientController.findOne);
 router.get('/checkDuplicateUsername/:username', verifyToken, patientController.checkDuplicateUsername);
+router.get('/:username', verifyToken, verifyRoles(["EDITOR", "ADMIN"]), patientController.findOne);
 router.post('/', verifyToken, verifyRoles(["ADMIN"]), patientController.create);
 router.patch('/:username', verifyToken, verifyRoles(["ADMIN"]),  patientController.update);
 router.delete('/:username', verifyToken, verifyRoles(["ADMIN"]), patientController.deleteByUsername);
--- backend/routes/visitor.routes.js
+++ backend/routes/visitor.routes.js
@@ -6,8 +6,8 @@ const verifyToken = require('../middlewares/auth.middleware').verifyToken;
 const verifyRoles = require('../middlewares/auth.middleware').verifyRoles;
 
 router.get('/', verifyToken, visitorController.findAll);
-router.get('/:username', verifyToken, verifyRoles(["EDITOR", "ADMIN"]), visitorController.findOne);
 router.get('/checkDuplicateUsername/:username', verifyToken, visitorController.checkDuplicateUsername);
+router.get('/:username', verifyToken, verifyRoles(["EDITOR", "ADMIN"]), visitorController.findOne);
 router.post('/', verifyToken, verifyRoles(["ADMIN"]), visitorController.create);
 router.patch('/:username', verifyToken, verifyRoles(["ADMIN"]),  visitorController.update);
 router.delete('/:username', verifyToken, verifyRoles(["ADMIN"]), visitorController.deleteByUsername);
