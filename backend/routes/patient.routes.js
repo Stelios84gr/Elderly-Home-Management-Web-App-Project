12b9224a693beb5f76bba1c5898985901444dceb
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-08-30 20:14:09 +0300

    fix: verification middleware added back

--- backend/routes/patient.routes.js
+++ backend/routes/patient.routes.js
@@ -7,7 +7,7 @@ const verifyRoles = require('../middlewares/auth.middleware').verifyRoles;
 
 router.get('/', verifyToken, patientController.findAll);
 // checkDuplicate GET route first to avoid "route shadowing" (specific ones first, more generic ones after)
-router.get('/checkDuplicateUsername/:username', patientController.checkDuplicateUsername);
+router.get('/checkDuplicateUsername/:username', verifyToken, verifyRoles(["ADMIN"]), patientController.checkDuplicateUsername);
 router.get('/:username', verifyToken, verifyRoles(["EDITOR", "ADMIN"]), patientController.findOne);
 router.post('/', verifyToken, verifyRoles(["ADMIN"]), patientController.create);
 router.patch('/:username', verifyToken, verifyRoles(["ADMIN"]),  patientController.update);
--- backend/routes/staff.routes.js
+++ backend/routes/staff.routes.js
@@ -7,8 +7,8 @@ const verifyRoles = require('../middlewares/auth.middleware').verifyRoles;
 
 router.get('/', verifyToken, staffController.findAll);
 // checkDuplicate GET routes first to avoid "route shadowing" (specific ones first, more generic ones after)
-router.get('/checkDuplicateUsername/:username', staffController.checkDuplicateUsername);
-router.get('/checkDuplicateEmail/:email', staffController.checkDuplicateEmail);
+router.get('/checkDuplicateUsername/:username', verifyToken, verifyRoles(["ADMIN"]),staffController.checkDuplicateUsername);
+router.get('/checkDuplicateEmail/:email', verifyToken, verifyRoles(["ADMIN"]),staffController.checkDuplicateEmail);
 router.get('/:username', verifyToken, verifyRoles(["EDITOR", "ADMIN"]), staffController.findOne);
 router.post('/', verifyToken, verifyRoles("ADMIN"), staffController.create);
 router.patch('/:username', verifyToken, verifyRoles("ADMIN"),  staffController.update);
