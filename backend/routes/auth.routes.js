a061d47204534cf4703b30b6a9aaf4db166b703d
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-08-31 17:28:45 +0300

    minor: changed google login url to match contemporary google requirements

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -8,7 +8,7 @@ router.get('/google/callback', authController.googleLogin);
 module.exports = router;
 
 // Google Sign-In URL:
-// https://accounts.google.com/o/oauth2/auth + path parameters
+// https://accounts.google.com/o/oauth2/v2/auth + path parameters
 
 // path parameters:
 // client_id = ...
@@ -17,4 +17,4 @@ module.exports = router;
 // scope = email%20profile
 // access_type = offline
 
-// https://accounts.google.com/o/oauth2/auth?client_id=[...]]&redirect_uri=[...]&response_type=code&scope=email%20profile&access_type=offline
+// https://accounts.google.com/o/oauth2/v2/auth?client_id=[...]]&redirect_uri=[...]&response_type=code&scope=email%20profile&access_type=offline
