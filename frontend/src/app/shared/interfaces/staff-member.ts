ff9cc7057e0a372646bd6c6b06a72cab8f09df07
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-09-01 19:25:54 +0300

    feat: staff member interface added

---
+++ frontend/src/app/shared/interfaces/staff-member.ts
@@ -0,0 +1,18 @@
+export interface StaffMember {
+    username: string,
+    password: string,
+    firstName: string,
+    lastName: string,
+    TIN: string,
+    phoneNumber: number,
+    address: Address,
+    occupation: string,
+    roles: string[],
+    startDate: Date,
+    monthlySalary: number
+};
+
+export interface Address {
+    road: string,
+    number: string
+};
