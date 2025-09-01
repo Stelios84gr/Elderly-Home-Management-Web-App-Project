d0a54923747ce6ed3513e657e08d29b7b3a0cda2
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-09-01 22:21:46 +0300

    feat: staffTable route added

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { StaffTable } from './components/staff-table/staff-table';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'staff-table', component: StaffTable}
+];
