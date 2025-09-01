a22f9d93e202b4b806df3dee1fef61a01a771e04
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-09-01 19:22:46 +0300

    feat: basic header layout added

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,15 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
+import { Navbar } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('frontend');
+  
 }
