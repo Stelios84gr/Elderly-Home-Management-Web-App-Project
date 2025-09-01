2f128c78883458d6dba32eaee3cb0b424fb131e5
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-09-01 22:20:03 +0300

    minor: RouterModule replaced with RouterLink & RouterActive

--- frontend/src/app/components/navbar/navbar.ts
+++ frontend/src/app/components/navbar/navbar.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule, MatToolbarModule, MatButtonModule],
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive, MatToolbarModule, MatButtonModule],
   templateUrl: './navbar.html',
   styleUrls: ['./navbar.css']
 })
 export class Navbar {
-
+  
 }
