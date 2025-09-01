47b6fd89b9148a124ddf08471143a506355fbb15
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-09-01 20:19:41 +0300

    feat: staff datatable component created

---
+++ frontend/src/app/components/staff-table/staff-table.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-staff-table',
+  imports: [],
+  templateUrl: './staff-table.html',
+  styleUrl: './staff-table.css'
+})
+export class StaffTable {
+
+}
