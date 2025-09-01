90fcf16bba90150be9d697cada9b3bdfd7cb4309
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-09-01 19:24:48 +0300

    feat: patient interface added

---
+++ frontend/src/app/shared/interfaces/patient.ts
@@ -0,0 +1,41 @@
+export interface Patient {
+    username: string,
+    firstName: string,
+    lastName: string,
+    AMKA: string,
+    dateOfBirth: Date,
+    phoneNumber: number,
+    authorizationToLeave: boolean,
+    roomData: RoomData,
+    patientAilments: [PatientAilment],
+    emergencyContactInfo: EmergencyContactInfo,
+    visitors: [Visitor]
+};
+
+export interface RoomData {
+     roomNumber: string,
+     bedNumber: string
+};
+
+export interface PatientAilment {
+    disease: string,
+    severity: number
+};
+
+export interface EmergencyContactInfo {
+    firstName: string,
+    lastName: string,
+    phoneNumber: number,
+    address: Address,
+    kinshipDegree: string
+};
+
+export interface Address {
+    road: string,
+    number: number
+};
+
+export interface Visitor {
+    _id: string,
+    relationship: string
+};
