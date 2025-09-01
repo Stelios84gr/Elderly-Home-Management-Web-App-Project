011f3808a034d818ef3d1063e5c4602009882a6d
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-09-01 22:24:20 +0300

    feat: onSubmit() implemented, output data & even emitter functionality added

--- frontend/src/app/components/staff-form/staff-form.ts
+++ frontend/src/app/components/staff-form/staff-form.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import {
   FormControl,
   FormGroup,
@@ -12,6 +12,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
+import { StaffMember } from 'src/app/shared/interfaces/staff-member';
 
 @Component({
   selector: 'app-staff-form',
@@ -25,10 +26,13 @@ import { MatButtonModule } from '@angular/material/button';
     MatNativeDateModule,
     MatButtonModule
   ],
+  standalone: true,
   templateUrl: './staff-form.html',
   styleUrls: ['./staff-form.css']
 })
 export class StaffForm {
+  @Output() staff = new EventEmitter<StaffMember>()
+
   staffForm = new FormGroup({
     username: new FormControl('', Validators.required),
     password: new FormControl('', Validators.required),
@@ -45,11 +49,33 @@ export class StaffForm {
     }),
     occupation: new FormControl('', Validators.required),
     roles: new FormControl([], Validators.required),
-    startDate: new FormControl('', Validators.required),
+    // makes sure startDate default value is never null or undefined
+    startDate: new FormControl(new Date(), Validators.required),
     monthlySalary: new FormControl('', Validators.required)
   });
 
   onSubmit() {
-
+    if (this.staffForm.valid) {
+      console.log("Valid Form:", this.staffForm.value);
+      const staffMember: StaffMember = {
+        username: this.staffForm.value.username ?? '',
+        password: this.staffForm.value.password ?? '',
+        firstName: this.staffForm.value.firstName ?? '',
+        lastName: this.staffForm.value.lastName ?? '',
+        TIN: this.staffForm.value.TIN ?? '',
+        phoneNumber: Number(this.staffForm.value.phoneNumber),
+        address: {
+          street: this.staffForm.value.address?.street ?? '',
+          number: this.staffForm.value.address?.number ?? ''
+        },
+        occupation: this.staffForm.value.occupation ?? '',
+        roles: this.staffForm.value.roles ?? [],
+        // asserts startDate is never null
+        startDate: new Date(this.staffForm.value.startDate!) ?? new Date(),
+        monthlySalary: Number(this.staffForm.value.monthlySalary)
+      };
+      this.staff.emit(staffMember);
+      this.staffForm.reset();
+    };
   };
 };
