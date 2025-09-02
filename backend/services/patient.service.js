7636eda7844a4e88c736aaaf25525716ed017e92
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-09-02 18:47:03 +0300

    feat: username auto-generator added

--- backend/services/patient.service.js
+++ backend/services/patient.service.js
@@ -1,5 +1,10 @@
 const Patient = require('../models/patient.model');
 
+// auto-generate username based on pattern
+function generatePatientUsername(firstName, lastName) {
+  return `1${lastName.charAt(0).toLowerCase()}${firstName.toLowerCase()}${lastName.length}`;
+};
+
 // return a promise, so async functions
 
 async function findAll() {
@@ -16,8 +21,12 @@ async function findOne({ username }) {
 
 async function create(data) {
 
+    // auto-generate username
+    const username = generatePatientUsername(data.firstName, data.lastName);
+
     const newPatient = new Patient({
-        username: data.username,
+        // not to be provided by the user
+        username,
         firstName: data.firstName,
         lastName: data.lastName,
         AMKA: data.AMKA,
@@ -48,6 +57,16 @@ async function create(data) {
 };
 
 async function update(username, data) {
+
+        const existing = await Patient.findOne({ username });
+        if (!existing) return null;
+    // username depends on firstName & lastName
+        if(data.firstName || data.lastName) {
+            data.username = generatePatientUsername(
+                data.firstName ?? existing.firstName,
+                data.lastName ?? existing.lastName
+            );
+        };
 
     const result = await Patient.findOneAndUpdate(
         { username },
