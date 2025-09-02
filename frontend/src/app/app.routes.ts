380d6e26e8f2c02c21071cf2ec6259ce589e1450
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-09-02 19:09:47 +0300

    fix: "staff-table" component import & route replaced with "staff-page"

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { StaffTable } from './components/staff-table/staff-table';
+import { StaffPage } from './components/staff-page/staff-page';
 
 export const routes: Routes = [
-    {path: 'staff-table', component: StaffTable}
+    {path: 'staff-page', component: StaffPage},
 ];
