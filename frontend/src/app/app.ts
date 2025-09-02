1e7e8e203471c492f3b18013a440c0bce87bd85d
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-09-02 19:11:46 +0300

    feat: RouterOutlet module import added

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Footer],
+  standalone: true,
+  imports: [
+    Header,
+    Footer,
+    RouterOutlet
+  ],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
