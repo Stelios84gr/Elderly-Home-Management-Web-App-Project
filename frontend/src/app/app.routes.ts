1dbd1b42b5b99f30b8b130516b532c31f690a274
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-09-03 11:54:39 +0300

    minor: redundant StaffPage route removed

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { StaffTable } from './components/staff-table/staff-table';
 import { StaffPage } from './components/staff-page/staff-page';
 
 export const routes: Routes = [
