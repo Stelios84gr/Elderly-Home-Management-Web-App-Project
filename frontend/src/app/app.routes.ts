b81a25bb0f0cf9ea3e0b7a21e0ef8a372ddf8b0e
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-09-06 19:48:50 +0300

    feat: login route added

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { StaffPage } from './components/staff-page/staff-page';
 import { PatientsPage } from './components/patients-page/patients-page';
 import { VisitorsPage } from './components/visitors-page/visitors-page';
+import { StaffLogin } from './components/login/login';
 
 export const routes: Routes = [
     {path: 'staff-page', component: StaffPage},
     {path: 'patients-page', component: PatientsPage},
-    {path: 'visitors-page', component: VisitorsPage}
+    {path: 'visitors-page', component: VisitorsPage},
+    {path: 'login', component: StaffLogin}
 ];
