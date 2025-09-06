fde0805e156652fe5b7d82b571c05a6fae8ed61d
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-09-06 20:37:28 +0300

    fix: email properly includeed in staffSchema definition & "road" changed to "street"

--- backend/models/staff.model.js
+++ backend/models/staff.model.js
@@ -3,15 +3,17 @@ const mongoose = require('mongoose')
 const schema = mongoose.Schema;
 
 let addressSchema = new schema({
-    road: { type: String, required: [true, '"road" is a required field'], minLength: 2, maxLength: 20, trim: true, lowercase: true},
-    number: { type: String, minLength:1, maxLength: 5, trim: true, uppercase: true}    // in case of "1150A", for example
-});
+    street: { type: String, required: [true, '"street" is a required field'], minLength: 2, maxLength: 20, trim: true, lowercase: true},
+    // in case of "1150A", for example
+    number: { type: String, required: [true, '"number is a required"'], minLength:1, maxLength: 5, trim: true, uppercase: true},
+}, {_id: false});
 
 let staffSchema = new schema({
     username: { type: String, required: [true, '"username" is a required field'], unique: true },
     password: { type: String, required: [true, '"password" is a required field'], minLength: 8, maxLength: 75 },
     firstName: { type: String, required: [true, '"firstName" is a required field'], minLength: 2, maxLength: 20, trim: true },
     lastName: { type: String, required: [true, '"lastName" is a required field'], minLength: 2, maxLength: 20, trim: true },
+    email: { type: String, required: [true, '"email" is a required field'], unique: true },
     TIN: { type: String, required: [true, '"TIN" is a required field'], minLength: 9, maxLength: 9, unique: true },    // in case it begins with 0
     phoneNumber: { type: Number, required: [true, '"phoneNumber" is a required field'], min: 1000000000, max: 9999999999, unique: true},    // min-max: ensures 10-digit format
     address: addressSchema,
