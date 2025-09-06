e6eda141974cf2fba8d75cf0cd8aee2956cdfeaa
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-09-06 19:54:12 +0300

    fix:  "welcome" message upon login added & styling adjusted

--- frontend/src/app/components/navbar/navbar.ts
+++ frontend/src/app/components/navbar/navbar.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterLinkActive, Router } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { StaffService } from 'src/app/shared/services/staff-service';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, MatToolbarModule, MatButtonModule],
+  imports: [RouterLink, RouterLinkActive, MatToolbarModule, MatButtonModule, MatIconModule, CommonModule],
   templateUrl: './navbar.html',
   styleUrls: ['./navbar.css']
 })
 export class Navbar {
-  
-}
+  router = inject(Router);
+  staffService = inject(StaffService);
+
+  // private: automatic Router instance injection
+  // constructor(private router: Router) {};
+
+  goToLogin() {
+    this.router.navigate(['/login']);
+  };
+
+  logout() {
+    this.staffService.logoutStaffMember();
+  };
+};
